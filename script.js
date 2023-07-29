d459738ba16cf63e53242eb34f7be32e3f0379d0
blehplayz <139347466+blehplayz@users.noreply.github.com>
2023-07-29 23:27:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,83 +3,59 @@ const products = [
   {
     id: 1,
     name: 'Product 1',
-    price:120.00,
+    price: 25.99,
   },
   {
     id: 2,
     name: 'Product 2',
-    price: 170.00,
+    price: 19.99,
   },
 ];
 
 // Function to display products on the page
 function displayProducts() {
-  const productContainer = document.querySelector('#products');
+  const productContainer = document.querySelector('main');
   productContainer.innerHTML = '';
 
   products.forEach((product) => {
     const productDiv = document.createElement('div');
+    productDiv.className = 'product';
     productDiv.innerHTML = `
-      <h3>${product.name}</h3>
+      <h2>${product.name}</h2>
+      <p>Description of ${product.name}.</p>
       <p>Price: $${product.price.toFixed(2)}</p>
-      <button onclick="buyProduct(${product.id})">Buy Now</button>
+      <button onclick="buyProduct('${product.name}')">Buy Now</button>
     `;
     productContainer.appendChild(productDiv);
   });
 }
 
-// Function to simulate GCash payment
-function payWithGCash(productID) {
-  return new Promise((resolve, reject) => {
-    // Simulate payment process
-    setTimeout(() => {
-      const success = Math.random() < 0.9; // 90% success rate
-      if (success) {
-        resolve(`Payment for Product ${productID} successful via GCash!`);
-      } else {
-        reject(new Error(`Payment for Product ${productID} failed via GCash.`));
-      }
-    }, 2000);
-  });
-}
-
-// Function to simulate PayPal payment
-function payWithPayPal(productID) {
-  return new Promise((resolve, reject) => {
-    // Simulate payment process
-    setTimeout(() => {
-      const success = Math.random() < 0.95; // 95% success rate
-      if (success) {
-        resolve(`Payment for Product ${productID} successful via PayPal!`);
-      } else {
-        reject(new Error(`Payment for Product ${productID} failed via PayPal.`));
-      }
-    }, 2000);
-  });
-}
-
-// Function to handle the payment based on the selected method
-async function buyProduct(productID) {
+// Function to handle the redirection to payment selection page
+function redirectToPayment(productName) {
   const paymentMethod = prompt(
-    'Choose a payment method: GCash or PayPal',
+    `Choose a payment method for ${productName}: GCash or PayPal`,
     'GCash'
   ).toLowerCase();
 
-  try {
-    switch (paymentMethod) {
-      case 'gcash':
-        const gcashMessage = await payWithGCash(productID);
-        alert(gcashMessage);
-        break;
-      case 'paypal':
-        const paypalMessage = await payWithPayPal(productID);
-        alert(paypalMessage);
-        break;
-      default:
-        throw new Error('Invalid payment method. Please choose GCash or PayPal.');
-    }
-  } catch (error) {
-    alert(error.message);
+  switch (paymentMethod) {
+    case 'gcash':
+      // Redirect to GCash payment selection page (replace with actual URL)
+      window.location.href = 'https://example.com/gcash-payment-page';
+      break;
+    case 'paypal':
+      // Redirect to PayPal payment selection page (replace with actual URL)
+      window.location.href = 'https://example.com/paypal-payment-page';
+      break;
+    default:
+      alert('Invalid payment method. Please choose GCash or PayPal.');
+  }
+}
+
+// Function to handle the payment based on the selected method
+async function buyProduct(productName) {
+  const confirmed = confirm(`Do you want to buy ${productName}?`);
+  if (confirmed) {
+    await redirectToPayment(productName);
   }
 }
 
