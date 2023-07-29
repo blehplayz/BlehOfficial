4fac0c5ada1e26e1668e14909c906e823bd20abc
blehplayz <139347466+blehplayz@users.noreply.github.com>
2023-07-30 01:11:07 +0800

    Create productData.js

---
+++ productData.js
@@ -0,0 +1,19 @@
+// Dummy product data
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    description: 'Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu purus ac arcu hendrerit tempor.',
+    price: 25.99,
+    images: ['product1-image1.jpg', 'product1-image2.jpg'],
+    comments: [],
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    description: 'Description of Product 2. Aliquam scelerisque, ex vel tempor sagittis, dolor sem sagittis metus.',
+    price: 19.99,
+    images: ['product2-image1.jpg', 'product2-image2.jpg'],
+    comments: [],
+  },
+];
